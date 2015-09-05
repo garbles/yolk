77ce48ce7993d6ccd7b605ef0c936df0543c5d1e
Gabe Scholz <gabe@brewhouse.io>
2015-09-05 13:08:10 -0700

    Allow components to be updated by rerendering component to same node

---
+++ test/examples/renderTwice-test.js
@@ -0,0 +1,42 @@
+/** @jsx createElement */
+
+const createElement = require(`createElement`)
+const eventHandler = require(`eventHandler`)
+const render = require(`render`)
+
+function Counter (props, children) {
+  const handlePlus = eventHandler(1)
+  const propCount = props.map(p => p.count)
+  const count = handlePlus.scan((x, y) => x+y, 0).combineLatest(propCount, (a,b) => a+b)
+
+  return (
+    <div>
+      <button id="plus" onclick={handlePlus}>+</button>
+      <span>Count: {count}</span>
+    </div>
+  )
+}
+
+describe(`rendering to the same element twice does not recreate the widget`, () => {
+
+  it(`only updates the props and or children`, () => {
+    const component = <Counter count={0} />
+    const node = document.createElement(`div`)
+    render(component, node)
+
+    assert.equal(node.innerHTML, `<div><button id="plus">+</button><span>Count: 0</span></div>`)
+
+    const plus = node.querySelector(`#plus`)
+
+    plus.click()
+    plus.click()
+
+    assert.equal(node.innerHTML, `<div><button id="plus">+</button><span>Count: 2</span></div>`)
+
+    const newComponent = <Counter count={5} />
+
+    render(newComponent, node)
+
+    assert.equal(node.innerHTML, `<div><button id="plus">+</button><span>Count: 7</span></div>`)
+  })
+})
