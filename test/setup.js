74a2e18cef6b498db86ba923190d6deac3057aae
Gabe Scholz <gabe@brewhouse.io>
2015-09-06 11:17:06 -0700

    Make sure to transfer all relevant props

--- test/setup.js
+++ test/setup.js
@@ -2,7 +2,7 @@ const jsdom = require('jsdom')
 const sinon = require('sinon')
 const chai = require('chai')
 
-global.document = jsdom.jsdom('<!doctype html><html><body></body></html>')
-global.window = document.parentWindow
+global.document || (global.document = jsdom.jsdom('<!doctype html><html><body></body></html>'))
+global.window || (global.window = document.parentWindow)
 global.sinon = sinon
 global.assert = chai.assert
