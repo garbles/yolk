163a3ffeda6b40691e6e189488c94be5622e2d62
Gabe Scholz <gabe@brewhouse.io>
2015-09-09 20:31:45 -0700

    Create specs for transformProperties

---
+++ test/unit/transformProperties-test.js
@@ -0,0 +1,36 @@
+const transformProperties = require(`transformProperties`)
+
+describe(`transformProperties`, () => {
+  it(`transforms props so that they will work correctly with virtual dom`, () => {
+    const onClick = function(){}
+
+    const props = {
+      className: `className`,
+      id: `id`,
+      acceptCharset: true,
+      dataTag: true,
+      dataOtherTag: true,
+      action: `/`,
+      rowSpan: 5,
+      onClick: onClick,
+      onmouseup: function () {},
+      nonStandardAttr: true
+    }
+
+    const transformedProps = {
+      className: `className`,
+      id: `id`,
+      style: undefined,
+      onclick: onClick,
+      attributes: {
+        action: `/`,
+        rowspan: 5,
+        'accept-charset': true,
+        'data-tag': true,
+        'data-other-tag': true
+      }
+    }
+
+    assert.deepEqual(transformProperties(props), transformedProps)
+  })
+})
