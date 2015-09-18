37919b98671a661e482082fe79ebab44fddd5842
Gabe Scholz <gabe@brewhouse.io>
2015-09-18 11:59:54 -0700

    Create transformProperty to transform individual properties

--- test/unit/transformStyle-test.js
+++ test/unit/transformStyle-test.js
@@ -2,10 +2,13 @@ const transformStyle = require(`../../lib/transformStyle`)
 
 describe(`transformStyle`, () => {
   it(`returns an empty object if style isn't defined in the props`, () => {
-    assert.deepEqual(transformStyle(undefined), {})
+    const props = {}
+    transformStyle(props, undefined)
+    assert.deepEqual(props.style, undefined)
   })
 
   it(`converts necessary number values in px values`, () => {
+    const props = {}
     const style = {
       height: 1,
       width: 1,
@@ -13,7 +16,9 @@ describe(`transformStyle`, () => {
       zIndex: 1000,
     }
 
-    assert.deepEqual(transformStyle(style), {
+    transformStyle(props, style)
+
+    assert.deepEqual(props.style, {
       height: `1px`,
       width: `1px`,
       lineHeight: 50,
