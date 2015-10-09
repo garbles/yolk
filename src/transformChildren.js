9905497ae673fe271441afe8ed5b495e8cb57af2
Gabe Scholz <gabe@brewhouse.io>
2015-10-09 11:35:42 -0700

    Guard for scenario where children are not an array

--- src/transformChildren.js
+++ src/transformChildren.js
@@ -1,7 +1,11 @@
 const wrapObject = require(`./wrapObject`)
 const hasToJS = require(`./hasToJS`)
 
+function toJSify (child) {
+  return hasToJS(child) ? child.toJS() : child
+}
+
 module.exports = function transformChildren (children) {
-  const _children = children.map(child => hasToJS(child) ? child.toJS() : child)
+  const _children = ([]).concat(children).map(toJSify)
   return wrapObject(_children)
 }
