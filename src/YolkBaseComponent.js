bcb56b8d2a462b5a73e831227409f0e260cf34ce
Gabe Scholz <gabe@brewhouse.io>
2015-10-20 19:13:50 -0700

    Move deletegator call to root component creator

--- src/YolkBaseComponent.js
+++ src/YolkBaseComponent.js
@@ -2,7 +2,6 @@ const Rx = require(`rx`)
 const create = require(`virtual-dom/create-element`)
 const diff = require(`virtual-dom/diff`)
 const h = require(`virtual-dom/h`)
-const Delegator = require(`dom-delegator`)
 const patch = require(`virtual-dom/patch`)
 const wrapObject = require(`./wrapObject`)
 const transformProperties = require(`./transformProperties`)
@@ -50,7 +49,6 @@ YolkBaseComponent.prototype = {
 
     const vNode = h(this.id)
     this._node = create(vNode)
-    Delegator(this._node)
 
     this._patchSubscription =
       Rx.Observable
--- src/YolkRootComponent.js
+++ src/YolkRootComponent.js
@@ -1,6 +1,7 @@
 const create = require(`virtual-dom/create-element`)
 const diff = require(`virtual-dom/diff`)
 const patch = require(`virtual-dom/patch`)
+const delegator = require(`dom-delegator`)
 
 const PREVIOUS_WIDGET_KEY = `__YOLK_PREVIOUS_WIDGET_KEY`
 
@@ -36,6 +37,7 @@ YolkRootComponent.render = function render (instance, node) {
   } else {
     child = create(root)
     node.appendChild(child)
+    delegator(node)
   }
 
   child[PREVIOUS_WIDGET_KEY] = root
