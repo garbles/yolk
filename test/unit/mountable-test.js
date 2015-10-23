704b0087c654fc73ba518f62c77aa50293da4b1d
Gabe Scholz <gabe@brewhouse.io>
2015-10-22 22:21:44 -0700

    Whoooooooops

--- test/unit/mountable-test.js
+++ test/unit/mountable-test.js
@@ -1,7 +1,7 @@
 const test = require(`tape`)
 const mountable = require(`mountable`)
 
-test.only(`mounting and unmounting an instance`, t => {
+test(`mounting and unmounting an instance`, t => {
   t.plan(2)
   t.timeoutAfter(100)
 
