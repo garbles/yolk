bf5c969a1a3b80fd51068bc825183c7b84d5175b
Gabe Scholz <gabe@brewhouse.io>
2015-10-30 15:11:32 -0700

    Fix build by timing out for more than one animation frame

--- test/examples/CreateCustomComponent-test.js
+++ test/examples/CreateCustomComponent-test.js
@@ -25,10 +25,13 @@ test(`creating a custom HTML5 element`, t => {
 
   const [node, cleanup] = createInstance()
 
+  // document.registerElement polyfill uses setTimeout(fn, 10) if requestAnimationFrame
+  // is not present. phantomJS does not use requestionAnimationFrame (?) so timeout for
+  // at least one frame
   setTimeout(() => {
     t.equal(node.outerHTML, `<create-custom height="10"><h1>hello world</h1></create-custom>`)
     cleanup()
-  }, 0)
+  }, 17)
 })
 
 test(`changing an attribute of a node should change internal Yolk instance`, t => {
@@ -39,17 +42,17 @@ test(`changing an attribute of a node should change internal Yolk instance`, t =
   let instance
 
   setTimeout(() => {
-    instance = node[`__YOLK_INSTANCE_KEY__`]
+    instance = node.__YOLK_INSTANCE_KEY__
     t.ok(instance instanceof YolkCompositeComponent)
     node.setAttribute(`height`, 500)
-  }, 0)
+  }, 17)
 
 
   setTimeout(() => {
-    const newInstance = node[`__YOLK_INSTANCE_KEY__`]
+    const newInstance = node.__YOLK_INSTANCE_KEY__
     t.ok(newInstance instanceof YolkCompositeComponent)
     t.notEqual(instance, newInstance)
     t.equal(node.outerHTML, `<create-custom height="500"><h1>hello world</h1></create-custom>`)
     cleanup()
-  }, 0)
+  }, 17)
 })
