3675eaf841df17406c3eb24e6b65ea3aea46df33
Gabe Scholz <gabe@brewhouse.io>
2015-11-07 16:11:06 -0800

    Remove undefined function

--- src/isComponent.js
+++ src/isComponent.js
@@ -3,5 +3,5 @@ const isVNode = require(`yolk-virtual-dom/vnode/is-vnode`)
 const isVText = require(`yolk-virtual-dom/vnode/is-vtext`)
 
 module.exports = function isComponent (obj) {
-  return !!obj && (isWidget(obj) || isThunk(obj) || isVNode(obj) || isVText(obj))
+  return !!obj && (isWidget(obj) || isVNode(obj) || isVText(obj))
 }
