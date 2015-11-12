91687fe0086ccd8685a1d3400a7b0b804d092c4d
Gabe Scholz <gabe@brewhouse.io>
2015-11-11 22:34:10 -0800

    Add hyperscript helper to Yolk

--- src/yolk.js
+++ src/yolk.js
@@ -4,10 +4,11 @@ const Rx = require(`rx`)
 const createElement = require(`./createElement`)
 const registerElement = require(`./registerElement`)
 const CustomComponent = require(`./YolkCustomComponent`)
+const h = require(`./h`)
 const render = require(`./YolkRootComponent`).render
 
 function Yolk () {}
-Yolk.prototype = {Rx, CustomComponent, createElement, registerElement, render}
+Yolk.prototype = {Rx, CustomComponent, createElement, h, registerElement, render}
 Object.freeze(Yolk)
 
 module.exports = new Yolk
--- test/examples/Counter-test.js
+++ test/examples/Counter-test.js
@@ -1,5 +1,6 @@
 const test = require(`tape`)
 const Yolk = require(`yolk`) // eslint-disable-line no-unused-vars
+const {h} = Yolk
 const renderInDoc = require(`../helpers/renderInDoc`)
 
 function Counter () {
@@ -7,13 +8,11 @@ function Counter () {
   const handleMinus = this.createEventHandler(-1)
   const count = handlePlus.merge(handleMinus).scan((x, y) => x + y, 0).startWith(0)
 
-  return (
-    <div>
-      <button id="plus" onClick={handlePlus}>+</button>
-      <button id="minus" onClick={handleMinus}>-</button>
-      <span>{count}</span>
-    </div>
-  )
+  return h(`div`, null, [
+    h(`button#plus`, {onClick: handlePlus}, `+`, ` PLUSSS`),
+    h(`button#minus`, {onClick: handleMinus}, [`-`, ` MINUSSS`]),
+    h(`span`, null, count),
+  ])
 }
 
 test(`Counter: a simple counter`, t => {
@@ -25,10 +24,10 @@ test(`Counter: a simple counter`, t => {
   t.equal(node.tagName, `DIV`)
   t.equal(node.children[0].tagName, `BUTTON`)
   t.equal(node.children[0].id, `plus`)
-  t.equal(node.children[0].innerHTML, `+`)
+  t.equal(node.children[0].innerHTML, `+ PLUSSS`)
   t.equal(node.children[1].tagName, `BUTTON`)
   t.equal(node.children[1].id, `minus`)
-  t.equal(node.children[1].innerHTML, `-`)
+  t.equal(node.children[1].innerHTML, `- MINUSSS`)
   t.equal(node.children[2].tagName, `SPAN`)
   t.equal(node.children[2].innerHTML, `0`)
 
