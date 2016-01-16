065d267f23f06fda63858080c05e8268b09de79f
Gabe Scholz <gabe@brewhouse.io>
2016-01-16 14:07:17 -0800

    Remove unused variable

--- src/dom/patchChildren.js
+++ src/dom/patchChildren.js
@@ -2,8 +2,6 @@
 
 import {createApplyPatch} from './createApplyPatch'
 
-const keyFn: Function = a => a.key
-
 export function patchChildren (node: HTMLElement, next: Array<VirtualNode>, previous?: Array<VirtualNode> = []): Array<VirtualNode> {
   return createApplyPatch(previous, next)(node)
 }
